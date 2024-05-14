3230ae43692daa574722f6a453dcbe217c51b4cc
mariia2206 <mariia.khudoian@gmail.com>
2024-05-14 23:08:08 +0300

    Добавление frontend части слайдера для отзывов

--- main/models.py
+++ main/models.py
@@ -1,9 +1,24 @@
 # Create your models here.
 from django.db import models
-
+# Класс для галереи
 class Photo(models.Model):
     image = models.ImageField(upload_to='photos/')
-    caption = models.CharField(max_length=100)
+    caption = models.CharField(max_length=255)
 
     def __str__(self):
         return self.caption
+
+# Класс поиска
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+
+class Review(models.Model):
+    author = models.CharField(max_length=100)
+    review_text = models.TextField()
+    photo = models.ImageField(upload_to='review_photos')
+
+    def __str__(self):
+        return self.author
+
--- main/views.py
+++ main/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from .models import Photo
+from .models import Product
+from .forms import SearchForm
+from .models import Review
+
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    reviews = Review.objects.all()
+    return render(request, 'main/index.html', {'reviews': reviews})
 
 def about(request):
     photos = Photo.objects.all()
@@ -13,4 +18,16 @@ def uslugi(request):
 
 def contacts(request):
     return render(request, 'main/contacts.html')
+
+def search(request):
+    if request.method == 'GET':
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            # Выполните поиск с использованием query, например:
+            results = Product.objects.filter(name__icontains=query)  # Замените на свой запрос
+            return render(request, 'main/search_results.html', {'results': results})
+    else:
+        form = SearchForm()
+    return render(request, 'main/search_form.html', {'form': form})
 

d9ce4b9fdf36ab09986c4eaae1f9d266a81fc7d1
mariia2206 <mariia.khudoian@gmail.com>
2024-06-05 23:17:06 +0300

    Изменение формы для записи

---
+++ main/static/shinka/js/valid.js
@@ -0,0 +1,6 @@
+
+        document.addEventListener('DOMContentLoaded', function() {
+            if ("{{ scroll_to_form }}") {
+                window.location.hash = '#booking_form';
+            }
+        });
--- main/views.py
+++ main/views.py
@@ -17,17 +17,36 @@ from django.shortcuts import render, redirect
 from .forms import ApplicationForm
 from .models import Service
 from django.contrib import messages
+from .models import Application
+from django.shortcuts import render, redirect
+
 
 def uslugi(request):
     services = Service.objects.all()
     actions = Action.objects.all()
+
     if request.method == 'POST':
         form = ApplicationForm(request.POST)
         if form.is_valid():
+            appointment_date = form.cleaned_data.get('appointment_date')
+            appointment_time = form.cleaned_data.get('appointment_time')
+
+            if Application.objects.filter(appointment_date=appointment_date, appointment_time=appointment_time,
+                                          approved=True).exists():
+                # Если время занято, оставляем пользователя на той же странице с формой и сообщением об ошибке
+                form.add_error('appointment_time', 'Это время уже занято. Пожалуйста, выберите другое время.')
+                return render(request, 'main/uslugi.html', {
+                    'actions': actions,
+                    'services': services,
+                    'form': form,
+                    'scroll_to_form': True
+                })
+
+            # Если время доступно, сохраняем заявку и отображаем страницу успеха
             form.save()
             send_mail(
                 'Спасибо за вашу заявку',
-                'Ваш заявка успешно получена. Мы ответим вам в ближайшее время.',
+                'Ваша заявка успешно получена. Мы ответим вам в ближайшее время.',
                 'your_email@example.com',  # Отправитель
                 [form.cleaned_data['email']],  # Получатель
                 fail_silently=False,
@@ -36,7 +55,10 @@ def uslugi(request):
     else:
         form = ApplicationForm()
 
-    return render(request, 'main/uslugi.html', {'actions': actions, 'services': services, 'form': form})
+    return render(request, 'main/uslugi.html',
+                  {'actions': actions, 'services': services, 'form': form, 'scroll_to_form': False})
+
+
 
 def success_view(request):
     return render(request, 'main/success.html')
